151b73e7eab0a38b050eff18567a19e316efa2a2
Mark <m@aqu.me.uk>
2017-05-17 18:04:00 +0100

    Even more things

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    private static Background background;
+    private PlayerController player;
+
+    void Awake() {
+        Background.background = this;
+    }
+
+    void Start()
+    {
+        player = PlayerController.GetPlayerController();
+        GetComponent<SpriteRenderer>().color = Color.green;
+    }
+
+    void Update() {
+        if (player != null) {
+            transform.position = player.transform.position * 0.2f;
+        }
+    }
+
+    public static Background GetBackground() {
+        return background;
+    }
+}
