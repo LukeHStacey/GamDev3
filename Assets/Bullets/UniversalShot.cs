1f40a135439030a961d4865fcfe88535f2133b0b
Mark <m@aqu.me.uk>
2017-05-18 16:44:12 +0100

    Added universal shot

--- Assets/Bullets/UniversalShot.cs
+++ Assets/Bullets/UniversalShot.cs
@@ -2,29 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpreadBullet : ShapeModifier {
+public class UniversalShot: ShapeModifier{
 
     public override List<B> OnFireBullet<B>(List<B> bulletList) {
         List<B> newBullets = new List<B>();
 
         foreach (B bullet in bulletList) {
             
-        Vector2 direction2 = Quaternion.Euler(0, 0, 15) * bullet.direction;
-        Vector2 direction3 = Quaternion.Euler(0, 0, -15) * bullet.direction;
+        Vector2 direction2 = Quaternion.Euler(0, 0, 90) * bullet.direction;
+        Vector2 direction3 = Quaternion.Euler(0, 0, 180) * bullet.direction;
+        Vector2 direction4 = Quaternion.Euler(0, 0, 270) * bullet.direction;
 
-        bullet.reloadTime = bullet.reloadTime * 2;
+        bullet.damage = bullet.damage/3;
         newBullets.Add(Bullet.FireBullet(direction2, bullet.transform.parent, bullet));
         newBullets.Add(Bullet.FireBullet(direction3, bullet.transform.parent, bullet));
+        newBullets.Add(Bullet.FireBullet(direction4, bullet.transform.parent, bullet));
 
         }
         newBullets.AddRange(bulletList);
         return newBullets;
     }
     public override string GetToolTip() {
-        return "+ Shots Fired";
+        return "+ Many Shots Fired";
     }
 
     public override int GetPriority() {
-        return 100;
+        return 50;
     }
 }
